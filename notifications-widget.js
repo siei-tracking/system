33c6060a204c3808bf186e4b5551cd69d7dfb8f9
siei  tracking <siei.traking2026@gmail.com>
2026-04-28 12:25:56 +0300

    Add files via upload

--- notifications-widget.js
+++ notifications-widget.js
@@ -683,8 +683,8 @@
           '.nw-lbtn{font-size:12px!important;padding:7px 11px!important;min-width:0!important;}' +
         '}' +
         '</style>' +
-        '<button id="btnInstallApp" class="nw-lbtn nw-install" type="button" style="display:none;">📲 تثبيت التطبيق</button>' +
-        '<button id="btnEnableNotifications" class="nw-lbtn nw-notif" type="button" style="display:none;">🔔 تفعيل الإشعارات</button>';
+        '<button id="btnEnableNotifications" class="nw-lbtn nw-notif" type="button" style="display:none;">🔔 تفعيل الإشعارات</button>' +
+        '<button id="btnInstallApp" class="nw-lbtn nw-install" type="button" style="display:none;">📲 تثبيت التطبيق</button>';
       header.appendChild(left);
     }
   }
