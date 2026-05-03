caced8598cb90fa96b6350572bb101bf01ea0ae8
siei  tracking <siei.traking2026@gmail.com>
2026-05-04 00:36:58 +0300

    Update API_BASE URL in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 ﻿window.APP_CONFIG = {
-  API_BASE: "https://script.google.com/macros/s/AKfycby4AxWqeuzJDsVjR9BrzzIwJLHy61rqX-19H-6juzDgZKPfAqCEsZjI3wHjJ4n3VnR2/exec",
+  API_BASE: "https://script.google.com/macros/s/AKfycbymlhMtUPb3-8QKua9YZgmUdP2mqz5VXzaibektsZSx6TwgV-rRpGjhzaiqtNC366Te/exec",
   VERSION: "1.0.3"
 };
