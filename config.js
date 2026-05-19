3a92a68ad6bd6796d479eb65d684604df1cc9a17
siei  tracking <siei.traking2026@gmail.com>
2026-05-19 17:24:20 +0300

    Update app version to 1.0.3 in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 ﻿window.APP_CONFIG = {
   API_BASE: "https://script.google.com/macros/s/AKfycbx_23E0QcwYwjPG1k5UAa3JdNray9LDnOroq0tXCDPYLgUe-DmqvBo4FBTp1ZD2MAf9/exec",
-  VERSION: "1.0.2"
+  VERSION: "1.0.3"
 };
