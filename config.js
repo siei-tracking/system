34c4044723e2dd5337d131d3e5c554a756a2a862
siei  tracking <siei.traking2026@gmail.com>
2026-06-03 21:20:53 +0300

    Update API_BASE URL and increment version

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 ﻿window.APP_CONFIG = {
-  API_BASE: "https://script.google.com/macros/s/AKfycby5l1Qj6eHa3VXz7rq-fovQh8ag4SxqfpEtDDHIGxKiGmcD3r4tBEmdultVWhvcpjdK/exec",
-  VERSION: "1.0.2"
+  API_BASE: "https://script.google.com/macros/s/AKfycbwch-YlADawhdxXW3WZs12VYV0y2UYqmzVOt8oYJ9h8FPI4RLqVifYRk90D3ER_KyN9/exec",
+  VERSION: "1.0.3"
 };
